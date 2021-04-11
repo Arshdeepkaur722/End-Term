401bbba68321651590d8cf2331a8e6618ab0e72d
Arshdeep kaur <arshdeep1925.cse18@chitkara.edu.in>
2021-04-11 17:47:24 +0530

    code

---
+++ script.js
@@ -0,0 +1,35 @@
+function showData() {
+    event.preventDefault();
+    var country = document.getElementById("value").value;
+    var start = document.getElementById("date1").value;
+    var end = document.getElementById("date2").value;
+
+    var confirmed = document.getElementById("confirmed");
+    var active = document.getElementById("active");
+    var deaths = document.getElementById("deaths");
+
+    if (country == '' || start == '' || end == '')
+        alert("enter the required field");
+
+    else {
+        var url = "https://api.covid19api.com/country/" + country + "?from=" + start + "T00:00:00Z&to=" + end + "T00:00:00Z";
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                var length = res.length;
+                var index = length - 1;
+
+                var c = res[index].Confirmed;
+                var a = res[index].Active;
+                var d = res[index].Deaths;
+
+                confirmed.innerHTML = c;
+                active.innerHTML = a;
+                deaths.innerHTML = d;
+
+                document.getElementById("res").style.display = "block";
+
+            })
+    }
+}
